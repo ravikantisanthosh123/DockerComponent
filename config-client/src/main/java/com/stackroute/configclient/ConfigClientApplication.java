e6a9a844dc5ef6c2b2de59da1624a2558928bce8
sravika1 <sravika1@in.ibm.com>
2019-03-19 17:39:34 +0530

    first commit

---
+++ config-client/src/main/java/com/stackroute/configclient/ConfigClientApplication.java
@@ -0,0 +1,28 @@
+package com.stackroute.configclient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class ConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClientApplication.class, args);
+	}
+}
+@RefreshScope
+@RestController
+class MessageRestController {
+
+	@Value("${msg:Hello world - Config Server is not working..pelase check}")
+	private String msg;
+
+	@RequestMapping("/msg")
+	String getMsg() {
+		return this.msg;
+	}
+}
